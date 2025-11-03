2843951acb4288c46f06787aed6ecbfbfa2594b8
gabrielalencar1999 <gabedaora@gmail.com>
2025-11-03 10:48:48 -0300

    Fix db_connect

--- src/api/config/conexaobase.php
+++ src/api/config/conexaobase.php
@@ -3,7 +3,7 @@
 	$servidor = MAIN_DB_HOST;
 	$user_conect = MAIN_DB_USER;
 	$senha = MAIN_DB_PASS;
-	$banco_conect = "info";
+	$banco_conect = MAIN_DB_SCHEMA;
 	$mysqli = new mysqli($servidor, $user_conect, $senha, $banco_conect);
 	
 
