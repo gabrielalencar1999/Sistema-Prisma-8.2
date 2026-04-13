3e261619b512de31d4ab64ff9136648c7ed1b3f1
Robson Sales <robsonlopessales@gmail.com>
2026-04-13 15:04:48 -0300

    feat: conexao

--- src/api/config/conexaobase.php
+++ src/api/config/conexaobase.php
@@ -1,9 +1,9 @@
 <?php
 
-	$servidor = MAIN_DB_HOST;
-	$user_conect = MAIN_DB_USER;
-	$senha = MAIN_DB_PASS;
-	$banco_conect = MAIN_DB_SCHEMA;
+	$servidor = MYSQL_HOST;
+	$user_conect = MYSQL_USER;
+	$senha = MYSQL_PASSWORD;
+	$banco_conect = MYSQL_DATABASE;
 	$mysqli = new mysqli($servidor, $user_conect, $senha, $banco_conect);
 	
 
