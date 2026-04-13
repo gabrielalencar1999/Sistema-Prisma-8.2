93f8716573551e34a78799424e649a971efa742d
Robson Sales <robsonlopessales@gmail.com>
2026-04-13 15:29:09 -0300

    feat: conexao

--- src/api/config/conexaobase.php
+++ src/api/config/conexaobase.php
@@ -1,9 +1,9 @@
 <?php
 
-	$servidor = 'prisma-legacy-mysql';
-	$user_conect = MYSQL_USER;
-	$senha = MYSQL_PASSWORD;
-	$banco_conect = MYSQL_DATABASE;
+	$servidor = MAIN_DB_HOST;
+	$user_conect = MAIN_DB_USER;
+	$senha = MAIN_DB_PASS;
+	$banco_conect = MAIN_DB_SCHEMA;
 	$mysqli = new mysqli($servidor, $user_conect, $senha, $banco_conect);
 	
 
